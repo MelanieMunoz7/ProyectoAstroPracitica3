ffe7f90173eb24d919ebd5c51c216625f8466448
MelanieMunoz7 <141173067+MelanieMunoz7@users.noreply.github.com>
2026-05-12 08:13:10 -0500

    Update CodigoEsclavo.ino

--- CodigoComentadoYEcuacionesQueUsa/CodigoEsclavo.ino
+++ CodigoComentadoYEcuacionesQueUsa/CodigoEsclavo.ino
@@ -1,60 +1,95 @@
+/*Código Esclavo*/
+
+//Liberías, una para comunciación con el serial y la otra para controlar los servos
 #include <SoftwareSerial.h>
 #include <Servo.h>
 
+//Serial virtual para la conexión con el Maestro
 SoftwareSerial maestro(2, 3);
+
+//Variables para los dos servos
 Servo servoH;
 Servo servoV;
 
-// Variables para suavizado
+//Variables para suavizado
 int targetH = 90;
 int targetV = 90;
+
+//Posiciones actuales
 float currentH = 90;
 float currentV = 90;
-float stepSize = 0.05; // AJUSTA ESTO: Menor número = más lento/suave
 
+//Paso para suaviazar el movimiento del motor
+float stepSize = 0.05; 
+
+//Ir guardando el mensaje del maestro
 String data = "";
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
+  //Comunicación con el maestro
   maestro.begin(9600);
-  
+
+  //Conexión de los servos
   servoH.attach(9);
   servoV.attach(10);
   
+  //Los servos empiezan en 90°
   servoH.write(currentH);
   servoV.write(currentV);
 }
 
-void loop() {
-  // 1. Leer datos del maestro
-  while (maestro.available()) {
+void loop() 
+{
+  /* 1. Leer datos del maestro*/
+
+  //Mientras para analizar si llegó info al serial
+  while (maestro.available()) 
+  {
+    //Lee un mensaje recibido
     char c = maestro.read();
-    if (c == '\n') {
+
+    //Verifica que el caracter recibido e sun salto de línea
+    if (c == '\n') 
+    {
+      //busca la posición de la coma dentro del mesnaje
       int commaIndex = data.indexOf(',');
-      if (commaIndex != -1) {
+      //Si encontro la coma correctamente
+      if (commaIndex != -1) 
+      {
+        //Toma la parte antes de la coma y la vuelve entero
         targetH = data.substring(0, commaIndex).toInt();
         targetV = data.substring(commaIndex + 1).toInt();
       }
+      //Limpia memoria para recibir el siguinete mensaje
       data = "";
-    } else {
+    } 
+    //Si todavía no llega el salto del línea  
+    else 
+    {
       data += c;
     }
   }
 
-  // 2. Lógica de suavizado (Movimiento incremental)
-  // Movimiento Horizontal
-  if (abs(targetH - currentH) > 0.1) {
+  /* 2. Lógica de suavizado (Movimiento incremental)*/
+  //Movimiento Horizontal
+  if (abs(targetH - currentH) > 0.1) 
+  {
+    //Aumentando poco a poco el ángulo
     if (currentH < targetH) currentH += stepSize;
     else currentH -= stepSize;
     servoH.write((int)currentH);
   }
 
-  // Movimiento Vertical
-  if (abs(targetV - currentV) > 0.1) {
+  //Movimiento Vertical
+  if (abs(targetV - currentV) > 0.1) 
+  {  
+    //Aumentando poco a poco el ángulo
     if (currentV < targetV) currentV += stepSize;
     else currentV -= stepSize;
     servoV.write((int)currentV);
   }
 
-  delay(15); // Pequeña pausa para dar tiempo al servo y controlar la velocidad total
+  delay(15); //Pequeña pausa para dar tiempo al servo y controlar la velocidad total
 }
